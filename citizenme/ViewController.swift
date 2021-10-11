b3aeb7e2fa12f249bf75099704eed7231ef57077
Furkan Karakoc <furkan.karakoc@albarakatech.com>
2021-10-12 01:56:36 +0300

    Initial Commit

---
+++ citizenme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  citizenme
+//
+//  Created by Furkan Karakoc on 12.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
